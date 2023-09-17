1e8ecb5cad27908f5d4929869b3522d40325aaee
FAISAL YUDHA NUGRAHA <yudhasaurus@gmail.com>
2023-09-17 08:38:55 +0700

    logo

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -2,7 +2,7 @@ import { logo } from "../../assets";
 
 const Logo = () => {
   return (
-    <a href="/" className="esm:px-0 lg:px-4 py-5">
+    <a href="#" className="esm:px-0 lg:px-4 py-5">
       <img src={logo} alt={logo} className="w-20" />
     </a>
   );
